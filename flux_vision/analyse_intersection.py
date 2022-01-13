7164a0f6ed0b1ee8f852c6c2584b6a09774b1b53
RaphaelIENCO <raphaelienco@gmail.com>
2022-01-13 18:09:05 +0100

    Ajout heat_map

--- flux_vision/analyse_intersection.py
+++ flux_vision/analyse_intersection.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 import pandas as pd
 from matplotlib import pyplot as plt
-
+from folium import Map
+from folium.plugins import HeatMapWithTime
+import webbrowser
 
 # Fonction de modif destypes en valeur int
 def mapType(type):
@@ -32,6 +36,7 @@ def mapZone(zone):
     else:
         return "Commune Montreuil"
 
+
 #Affichage
 pd.set_option('display.width', 500)
 pd.set_option('display.max_columns', None)
@@ -50,7 +55,7 @@ df['Zone'] = df.Zone.apply(mapZone)
 #Plus rapide :
 # print("requete start : ")
 #je retire Commune monteuil car trop large
-dfTravail =  df[(df['Type'] == 0) & (df['Date'] >= "2020-04-20") & (df['Date'] <= "2020-04-26")  & (df['ZoneNuitee'] != -1) & (df.Zone != 'Commune Montreuil')]
+dfTravail =  df[(df['Type'] == 0) & (df['Date'] >= "2020-04-20") & (df['Date'] <= "2020-04-27")  & (df['ZoneNuitee'] != -1) & (df.Zone != 'Commune Montreuil')]
 dfTravail = dfTravail.sort_values(['Zone', 'Date','Heures'])
 dfTravail.columns = dfTravail.columns.str.strip()
 print(dfTravail.head(100))
@@ -105,3 +110,52 @@ def createHistoVolumeParHeure(df,zone):
 # createHistoVolumeParHeure(dfTravail[dfTravail['ZoneNuitee'] == 535976],"la Noue Clos Francais Guilands 1")
 # createHistoVolumeParHeure(dfTravail[dfTravail['ZoneNuitee'] == 535976],"Centre Ville Jean Moulin Espoir 4")
 
+# HEATMAP :
+
+dfTravail['lat']=0.0
+dfTravail['lon']=0.0
+dfTravail.loc[(dfTravail.Zone == "Bas Montreuil Est 4"),'lat'] = 48.8580927822
+dfTravail.loc[(dfTravail.Zone == "Bas Montreuil Est 4"),'lon'] = 2.43087582343
+dfTravail.loc[(dfTravail.Zone == "Bas Montreuil Est 5"),'lat'] = 48.8600096252
+dfTravail.loc[(dfTravail.Zone == "Bas Montreuil Est 5"),'lon'] = 2.43575229837
+dfTravail.loc[(dfTravail.Zone == "Bas Montreuil Est 6"),'lat'] = 48.858509261
+dfTravail.loc[(dfTravail.Zone == "Bas Montreuil Est 6"),'lon'] = 2.43831180464
+dfTravail.loc[(dfTravail.Zone == "la Noue Clos Francais Guilands 1"),'lat'] = 48.8614701175
+dfTravail.loc[(dfTravail.Zone == "la Noue Clos Francais Guilands 1"),'lon'] = 2.42951750824
+dfTravail.loc[(dfTravail.Zone == "Centre Ville Jean Moulin Espoir 4"),'lat'] = 48.8608990488
+dfTravail.loc[(dfTravail.Zone == "Centre Ville Jean Moulin Espoir 4"),'lon'] = 2.44076046517
+
+
+map =  Map(location=[48.861099,2.443600],zoom_start=16)
+
+#print(list(zip(dfTravail.lat, dfTravail.lon, dfTravail.Volume)))
+dfHeatMap1J = dfTravail[ (dfTravail['ZoneNuitee'] == 535976)]
+
+dfHeatMap1J.Volume = dfHeatMap1J.Volume / dfHeatMap1J.Volume.max()
+
+index_timestamp = []
+for row in dfHeatMap1J.groupby(['Date','Heures']):
+    #row[1]  -> Series avec nos 4 lignes, iloc[0] pour recuperer seulement la premiere ligne car les 4 ont le mm timestamp
+    #index_timestamp.append([datetime.strptime((row[1].iloc[0]['Date'] + " " +row[1].iloc[0]['Heures']),'%Y-%m-%d %H:%M:%S').timestamp()])
+    index_timestamp.append([(row[1].iloc[0]['Date'] + " " +row[1].iloc[0]['Heures'])])
+
+data = []
+
+for _, d in dfHeatMap1J.groupby(['Date','Heures']):
+    data.append([[row['lat'], row['lon'], row['Volume']] for _, row in d.iterrows()])
+print(index_timestamp)
+print(data)
+
+
+hm_wide = HeatMapWithTime(
+    data,
+    index = index_timestamp,
+    gradient={0.000:'blue',0.1:'green',0.45:'yellow',0.55:'orange',0.7:'red'},
+    min_opacity=0.75,
+    radius=60,
+)
+
+map.add_child(hm_wide)
+
+map.save("map.html")
+webbrowser.open("map.html")
